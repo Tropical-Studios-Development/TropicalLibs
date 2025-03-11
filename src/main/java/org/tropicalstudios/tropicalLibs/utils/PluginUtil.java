d4c3c95cdaac9a52bb0a4a20273cb4722c4adfa4
GameTheEnder <gametheendermanagement@gmail.com>
2025-03-11 16:55:11 +0200

    .

--- src/main/java/org/tropicalstudios/tropicalLibs/utils/PluginUtil.java
+++ src/main/java/org/tropicalstudios/tropicalLibs/utils/PluginUtil.java
@@ -7,27 +7,22 @@ import java.lang.reflect.Field;
 
 public class PluginUtil {
 
-    public static Plugin getCallingPlugin() {
-        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-        // Start at index 2 to skip getCallingPlugin and getStackTrace methods
-        for (int i = 2; i < stackTrace.length; i++) {
-            String className = stackTrace[i].getClassName();
-            try {
-                Class<?> clazz = Class.forName(className);
-                ClassLoader classLoader = clazz.getClassLoader();
+    public static void setPluginInstance(Plugin plugin) {
+        try {
+            // Get the ChatUtil class
+            Class<?> chatUtilClass = Class.forName("org.tropicalstudios.tropicalLibs.TropicalLibs");
 
-                // Check all loaded plugins
-                for (Plugin plugin : Bukkit.getPluginManager().getPlugins()) {
-                    if (plugin.getClass().getClassLoader() == classLoader) {
-                        return plugin;
-                    }
-                }
-            } catch (ClassNotFoundException e) {
-                // Skip if class can't be found
-                continue;
-            }
+            // Get the customPrefix field
+            Field customPrefixField = chatUtilClass.getDeclaredField("INSTANCE");
+
+            // Make it accessible even if it's private
+            customPrefixField.setAccessible(true);
+
+            // Set your custom prefix
+            customPrefixField.set(null, plugin);
+        } catch (Exception e) {
+            Bukkit.getLogger().severe("Failed to set plugin instance: " + e.getMessage());
         }
-        return null; // Return null if no plugin found
     }
 
     public static void setPluginPrefix(String prefix) {
